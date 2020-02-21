a42f58e39752749c68346805430a5135ef1888ae
Matt Blodgett <31248374+thailandmatt@users.noreply.github.com>
2020-02-21 13:28:02 -0700

    updated

---
+++ v2/displayinputcaterpillar/displayinputcaterpillar_test.go
@@ -0,0 +1,12 @@
+package displayinputcaterpillar
+
+import (
+	"testing"
+
+	"github.com/byuoitav/common/log"
+)
+
+func Test(t *testing.T) {
+	log.SetLevel("debug")
+	StartDisplayInputCaterpillar()
+}
--- v2/server.go
+++ v2/server.go
@@ -1,35 +1,11 @@
 package main
 
 import (
-	"net/http"
-	
-	"github.com/byuoitav/common/log"	
-	"github.com/labstack/echo"
+	"github.com/byuoitav/caterpillar/v2/displayinputcaterpillar"
+	"github.com/byuoitav/common/log"
 )
 
-const port = ":10012"
-
 func main() {
-	//log.SetLevel("debug")
-	
-	
-
-	router := echo.New()
-
-	router.GET("/status", getStatus)
-
-	server := http.Server{
-		Addr:           port,
-		MaxHeaderBytes: 1024 * 10,
-	}
-
-	router.StartServer(&server)
-}
-
-func getStatus(context echo.Context) error {
-	log.L.Debug("Getting status")
-	status := ""
-	log.L.Debugf("Status: %v", status)
-
-	return context.JSON(http.StatusOK, status)
+	log.SetLevel("debug")
+	displayinputcaterpillar.StartDisplayInputCaterpillar()
 }
